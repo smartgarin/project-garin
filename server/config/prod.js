e3aa714ac1a6edd514749f0f99c9da8afe2e19f1
정연한 <yt4307@gmail.com>
2021-11-04 12:21:53 +0900

    재설정중...

--- server/config/prod.js
+++
@@ -1,5 +0,0 @@
-const prodkey = {
-    mongoURI: process.env.MONGO_URI
-}
-
-export default prodkey;
--- server/routers/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/", (req, res) => {
-    res.send({ api: "node.js 서버에서 프론트엔드로 넘어온 내용입니다." });
-});
-
-module.exports = router;
