22fdc5f7172c22ad0b6d583588051c615a87ae33
정연한 <yt4307@gmail.com>
2021-11-05 12:15:26 +0900

    프론트엔드 페이지 스타일 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect } from "react";
-import './App.css';
+import style from "./App.module.scss";
 
 const App = () => {
   const callApi = async () => {
@@ -12,8 +12,18 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
-      test
+    <div className={style.App}>
+      <div><h1>가린</h1></div>
+      <div>
+        
+        <div className={style.topMenu}>
+          <span className={style.left}>사용자등록</span>
+          <span className={style.right}>로그인</span>
+        </div>
+        <div className={style.menu}>우리집 상태 확인</div>
+        <div className={style.menu}>집 안 제어</div>
+        <div className={style.menu}>가린 호출</div>
+      </div>
     </div>);
 }
 
